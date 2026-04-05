11970c18efeaed8ba9a64f4a6d96065fbcc67840
anielqa <anielqa@users.noreply.github.com>
2026-04-05 00:47:45 -0400

    Refactor background styles for header elements

--- physis-fixes.js
+++ physis-fixes.js
@@ -8,11 +8,11 @@
     + '.header__logo-wrapper{background:transparent!important;background-color:transparent!important;border:none!important;border-radius:0!important;box-shadow:none!important;margin:0 auto!important}'
     + '[class*="logo-wrapper"]{background:transparent!important;background-color:transparent!important;border:none!important}'
     + '.header__logo{background:transparent!important;background-color:transparent!important;display:flex!important;justify-content:center!important;width:100%!important}'
-    + '.w-block-header{background-color:transparent!important}'
-    + '.w-block-background{background-color:transparent!important}'
+    + '.w-block-background{background-color:transparent!important;background:transparent!important}'
+    + '.w-block-header{background-color:transparent!important;background:transparent!important}'
+    + '.w-header{background-color:transparent!important;background:transparent!important}'
     + '.header__flex{justify-content:center!important;width:100%!important}'
     + '.header__content-container{justify-content:center!important;width:100%!important}'
-    + '.header__condensed{justify-content:center!important}'
     + '}';
   document.head.appendChild(s);
 
@@ -24,16 +24,16 @@
       el.style.setProperty('margin', '0 auto', 'important');
     });
 
+    document.querySelectorAll('.w-block-background, .w-block-header, .w-header').forEach(function(el) {
+      el.style.setProperty('background-color', 'transparent', 'important');
+      el.style.setProperty('background', 'transparent', 'important');
+      el.style.setProperty('--color-white', 'transparent', 'important');
+    });
+
     var l = document.querySelector('.w-sitelogo');
     var i = document.querySelector('.w-sitelogo img');
     if (l) l.style.setProperty('--mobile-width', '160px', 'important');
     if (i) i.style.setProperty('width', '160px', 'important');
-
-    var header = document.querySelector('.header__flex, .header__content-container');
-    if (header) header.style.setProperty('justify-content', 'center', 'important');
-
-    var bg = document.querySelector('.w-block-background');
-    if (bg) bg.style.setProperty('background-color', 'transparent', 'important');
   }
 
   var observer = new MutationObserver(function() {
