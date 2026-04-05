55c087a2374f486a5d0e34f598e99954e7b604bb
anielqa <anielqa@users.noreply.github.com>
2026-04-05 00:07:18 -0400

    Update CSS styles for header and logo elements

--- physis-fixes.js
+++ physis-fixes.js
@@ -5,20 +5,23 @@
     + '.header__logo{--mobile-width:160px!important}'
     + '.w-sitelogo[data-v-23d6841e]{--mobile-width:160px!important}'
     + '.w-sitelogo[data-v-23d6841e] img{width:160px!important;max-width:160px!important;min-width:160px!important}'
-    + '.w-block-background{background-color:transparent!important}'
-    + '.w-block-header{background-color:transparent!important}'
-    + '.w-header{background-color:transparent!important}'
-    + '.header__content-container{background-color:transparent!important}'
-    + '[style*="--color-white"]{--color-white:transparent!important}'
+    + '.header__logo-wrapper[data-v-fd76376c]{background:transparent!important;border:none!important;border-radius:0!important;box-shadow:none!important;padding:0!important}'
+    + '.header__logo-wrapper{background:transparent!important;border:none!important;border-radius:0!important;box-shadow:none!important}'
     + '}';
   document.head.appendChild(s);
 
   function fixLogo() {
     var l = document.querySelector('.w-sitelogo');
     var i = document.querySelector('.w-sitelogo img');
+    var w = document.querySelector('.header__logo-wrapper');
     if (l && i) {
       l.style.setProperty('--mobile-width', '160px', 'important');
       i.style.setProperty('width', '160px', 'important');
+    }
+    if (w) {
+      w.style.setProperty('background', 'transparent', 'important');
+      w.style.setProperty('border', 'none', 'important');
+      w.style.setProperty('border-radius', '0', 'important');
       clearInterval(t);
     }
   }
